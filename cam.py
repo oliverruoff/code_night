4c5bb1c4e4378ef081fb3fbec2250b9988f778db
Oliver Ruoff <oliver93ruoff@web.de>
2018-12-22 01:06:30 +0100

    added gitignore

--- cam.py
+++ cam.py
@@ -1,6 +1,9 @@
 from cv2 import VideoCapture, namedWindow, imshow, waitKey, destroyWindow, imwrite
+import cv2
+import argparse
+import numpy as np
 
-FRAME_SIZE = 500
+FRAME_SIZE = 50
 
 # initialize the camera
 cam = VideoCapture(0)   # 0 -> index of camera
